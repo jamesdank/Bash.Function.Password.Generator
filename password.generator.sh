95b2ef10e8ff571cceb5a38e6872bba7618aaacc
jamesdank <94645949+jamesdank@users.noreply.github.com>
2023-12-19 19:33:42 -0600

    Update password.generator.sh

--- password.generator.sh
+++ password.generator.sh
@@ -3,10 +3,12 @@
 function password () {
     echo -e "\n\e[4mPassword Length\e[0m "
     read -p "Length: " length
+
     lower="abcdefghijklmnopqrstuvwxyz"
     upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     numbers="0123456789"
     symbols="/\~!@#$%^&*'()_+={[}]|<,>.?"
     password=$(< /dev/urandom tr -dc ${lower}${upper}${numbers}${symbols} | head -c${1:-$length};)
+    
     echo Password: $password
 }
